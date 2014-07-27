02a42cd66cce01e7dcaca933384e12b3fc50b4b9
Ryan Nhieu <ryan.nhieu@gmail.com>
2014-07-26 22:28:17 -0400

    used simple modals, want to change to more cohesive colors later, sign up is left

--- action.js
+++ action.js
@@ -1,15 +1,28 @@
 function help() {
-    if () {
         var helpPop = document.createElement("div");
         document.body.appendChild(helpPop);
-        
-    }
 }
 
 function login() {
+    var loading = $("#info");
+    loading.toggle();
+    $('#signup').prop('disabled', true);
+    $('#help').prop('disabled', true);
+    $('#login').button('loading');
+    setTimeout(function(){
+        loading.toggle();
+        $('#login').button('reset');
+        $('#signup').prop('disabled', false);
+        $('#help').prop('disabled', false);
+        $('#success').modal('show');
+    },3000);
     
 }
 
 function signUp() {
     
+}
+
+function openGlass() {
+
 }
